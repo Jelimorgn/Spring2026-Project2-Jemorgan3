2cffd97819154d6304a5c5c3a670817cc58648f8
Jemorgan3 <Jemorgan3@crimson.ua.edu>
2026-03-02 14:30:27 -0600

    Initial ASP.NET project setup

---
+++ SemesterYear-Project2-Jemorgan3/SemesterYear-Project2-Jemorgan3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SemesterYear_Project2_Jemorgan3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
